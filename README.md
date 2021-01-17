7d988f666bf3877fc7a2b8a7c16b2be445ac29e3
ArjPat <arj.pat23@gmail.com>
2021-01-16 19:57:16 -0500

    vscode and powershell mod

---
+++ README.md
@@ -0,0 +1,3 @@
+# Config Files
+Personal configuration files
+* I lost my updated bash and vimrc after laptop got wiped check if ilab still has it
--- profile.ps1
+++ profile.ps1
@@ -6,5 +6,25 @@ Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
 
-# New-Alias <alias> <aliased-command>
+Import-Module posh-git
+Import-Module oh-my-posh
+Set-Theme Paradox
+
+# Aliases
 New-Alias open ii
+
+Function tail {
+    Get-Content -Tail $args[0] $args[1] -Wait
+}
+
+Function serve {
+    $ids = @()
+    $ids += Start-Process -NoNewWindow -PassThru powershell 'tail 5 .\web\logs\index_php.log';
+    $ids += Start-Process -NoNewWindow -PassThru powershell 'php -S 127.0.0.1:8080 -t .\web\'
+    return $ids
+}
+
+#region conda initialize
+# !! Contents within this block are managed by 'conda init' !!
+(& "C:\Users\Arjun\Anaconda3\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | Invoke-Expression
+#endregion
